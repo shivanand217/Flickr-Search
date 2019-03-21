69e05d565f4d8c2cbe0d496cf02181b395c972c3
shiv anand <shivakp2111@gmail.com>
2019-03-21 10:51:51 +0530

    custom section header in collectionView

---
+++ flickr/FlickrPhotoHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoHeaderView.swift
+//  flickr
+//
+//  Created by apple on 21/03/19.
+//  Copyright © 2019 shiv. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoHeaderView: UICollectionReusableView {
+        
+}
